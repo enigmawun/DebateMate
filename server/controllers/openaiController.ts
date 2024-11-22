2ce89df4b74374f21f6833b047a9f1cf08388c83
Jeetp45 <141786979+Jeetp45@users.noreply.github.com>
2024-11-21 20:19:40 -0500

    assessment post route

--- server/controllers/openaiController.ts
+++ server/controllers/openaiController.ts
@@ -73,62 +73,63 @@ export const queryOpenAIArgument: RequestHandler = async (_req, res, next) => {
   }
 };
 
-// export const queryOpenAIEvaluation: RequestHandler = async (
-//   _req,
-//   res,
-//   next
-// ) => {
-//   const { debateHistoryFeedback, systemEvaluationContent } = res.locals;
+export const queryOpenAIEvaluation: RequestHandler = async (
+  _req,
+  res,
+  next
+) => {
+  const { debateHistoryFeedback, systemEvaluationContent } = res.locals;
 
-//   if (!debateHistoryFeedback) {
-//     const error: ServerError = {
-//       log: 'queryOpenAIEvaluation did not receive debate history feedba ',
-//       status: 500,
-//       message: { err: 'An error occurred before querying OpenAI for evaluation' },
-//     };
-//     return next(error);
-//   }
+  if (!debateHistoryFeedback) {
+    const error: ServerError = {
+      log: 'queryOpenAIEvaluation did not receive debate history feedback ',
+      status: 500,
+      message: {
+        err: 'An error occurred before querying OpenAI for evaluation',
+      },
+    };
+    return next(error);
+  }
 
-//   try {
-//     const response = await openai.chat.completions.create({
-//       model: 'gpt-4o-mini',
-//       messages: [
-//         {
-//           role: 'system',
-//           content: systemEvaluationContent,
-//         },
-//         {
-//           role: 'user',
-//           content: JSON.stringify(userArguments),
-//         },
-//       ],
-//       temperature: 0.7,
-//     });
-//     console.log('response: ', response.choices[0].message.content);
-//     const argumentParams = JSON.parse(response.choices[0].message.content!);
-//     if (argumentParams) {
-//       res.locals.aiArgument = argumentParams.ai_argument;
-//       console.log('aiArgument: ', res.locals.aiArgument);
-//       console.log('sucessfully queried OpenAI for argument');
-//       return next();
-//     } else {
-//       const apiError: ServerError = {
-//         log: 'OpenAI did not return an argument',
-//         status: 500,
-//         message: {
-//           err: 'An error occurred while querying OpenAI within queryOpenAIArgument',
-//         },
-//       };
-//       return next(apiError);
-//     }
-//   } catch (err) {
-//     const apiError: ServerError = {
-//       log: 'queryOpenAIArgument: Error: OpenAI error' + err,
-//       status: 500,
-//       message: { err: 'An error occurred while querying OpenAI' },
-//     };
-//     return next(apiError);
-//   }
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [
+        {
+          role: 'system',
+          content: systemEvaluationContent,
+        },
+        {
+          role: 'user',
+          content: JSON.stringify(debateHistoryFeedback),
+        },
+      ],
+      temperature: 0.7,
+    });
+    console.log('response: ', response.choices[0].message.content);
+    const evaluationResult = JSON.parse(response.choices[0].message.content!);
+    if (evaluationResult) {
+      res.locals.evaluationResult = evaluationResult;
+      console.log('debate assessment: ', res.locals.evaluationResult);
+      console.log('sucessfully queried OpenAI for debate assessment');
 
-//   return next();
-// };
+      return next();
+    } else {
+      const apiError: ServerError = {
+        log: 'OpenAI did not return an argument',
+        status: 500,
+        message: {
+          err: 'An error occurred while querying OpenAI within queryOpenAIArgument',
+        },
+      };
+      return next(apiError);
+    }
+  } catch (err) {
+    const apiError: ServerError = {
+      log: 'queryOpenAIArgument: Error: OpenAI error' + err,
+      status: 500,
+      message: { err: 'An error occurred while querying OpenAI' },
+    };
+    return next(apiError);
+  }
+};
